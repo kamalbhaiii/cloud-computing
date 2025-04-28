2667a9d0840a3f3199dd5f7bcb62bc6d15ac8f04
kamalbhaiii <kamal5201ks@gmail.com>
2025-04-28 21:03:10 +0200

    Server Initialization

---
+++ server/src/test/java/com/fra_uas/cloud_computing/CloudComputingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fra_uas.cloud_computing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudComputingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
