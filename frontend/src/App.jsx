9680d5660574ffeb5d6a1e351e99a7b51aac1cd2
kamalbhaiii <kamal5201ks@gmail.com>
2025-04-29 18:53:23 +0200

    Github Actions Image Creation test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Checking the github actions automated Image creation by Kamal
       </p>
     </>
   )
