c671c4b0540e78fbfd8a2bd458ae8e94030f50af
kamalbhaiii <kamal5201ks@gmail.com>
2025-05-01 17:35:27 +0200

    Frontend Base Change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Checking the github actions automated Image creation by Miss Varshitha with Authentication Testing Again
-      </p>
-    </>
+    <div className='h-screen w-full'>
+      <h3 className='text-center'>
+        Frontend
+      </h3>
+    </div>
   )
 }
 
