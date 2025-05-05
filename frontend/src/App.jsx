8eb9dcbe4b73198a5b350d1fe8991fe9a73967e9
kamalbhaiii <kamal5201ks@gmail.com>
2025-05-05 13:06:55 +0200

    Frontend-build-test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <div className='h-screen w-full'>
       <h3 className='text-center'>
-        Frontend
+        Frontend- added tailwindcss
       </h3>
     </div>
   )
