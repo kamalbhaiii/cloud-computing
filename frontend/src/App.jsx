a159c36162694ee9abfe9b2e9d0b3909423688db
kamalbhaiii <kamal5201ks@gmail.com>
2025-05-13 11:21:01 +0200

    Deployment Notification check

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <div className='h-screen w-full'>
       <h3 className='text-center'>
-        Frontend- added tailwindcss - Notification-Test-2
+        Frontend- added tailwindcss
       </h3>
     </div>
   )
