d8ccc8f53d40d18fd912c8e6732ea7d83d2d17bf
kamalbhaiii <kamal5201ks@gmail.com>
2025-06-15 20:18:33 +0200

    Model Dockerfile

--- Model/app-robo.py
+++ Model/app-robo.py
@@ -72,4 +72,4 @@ def handle_frame(base64_data):
         emit("prediction", {"prediction": None})
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(app, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
--- frontend/src/pages/camera/index.tsx
+++ frontend/src/pages/camera/index.tsx
@@ -2,7 +2,7 @@
 import { useEffect, useRef, useState } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://127.0.0.1:5000");
+const socket = io("http://192.168.137.178:30060");
 
 function App() {
   const videoRef = useRef<HTMLVideoElement | null>(null);
