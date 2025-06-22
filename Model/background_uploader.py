4a1c4547221ed0eca63c0c01a0c76588bf5c6dcc
kamalbhaiii <kamal5201ks@gmail.com>
2025-06-22 20:34:05 +0200

    Fixing Background Uploader

--- Model/background_uploader.py
+++ Model/background_uploader.py
@@ -1,16 +1,26 @@
+import mimetypes
+import os
 import requests
 
 def upload_image_to_db(image_path: str, category: str):
     print(image_path, category)
     url = "http://192.168.137.178:30070/api/images"
-    
+
+    mime_type, _ = mimetypes.guess_type(image_path)
+    if mime_type is None:
+        mime_type = "application/octet-stream"  # fallback
+
     with open(image_path, "rb") as img_file:
-        files = {"image": img_file}  # Proper binary file upload
-        data = {"category": category}  # Form data
-        
+        files = {
+            "image": (os.path.basename(image_path), img_file, mime_type)
+        }
+        data = {"category": category}
+
         try:
             response = requests.post(url, files=files, data=data)
             response.raise_for_status()
             print(f"[INFO] Image uploaded successfully: {category}")
+            os.remove(image_path)
         except requests.RequestException as e:
             print(f"[ERROR] Failed to upload image: {e.response.text}")
+            os.remove(image_path)
